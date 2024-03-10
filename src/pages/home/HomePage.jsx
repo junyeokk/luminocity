a366fd58fdb008dddc1ba5ebf9cc3639b8d0432d
junyeokk <cjh4302@gmail.com>
2024-03-11 00:57:31 +0900

    불필요한 코드제거

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,17 +1,7 @@
-import styled from "styled-components";
 import Map from "../../components/map/Map";
 
-const HomePageContainer = styled.div`
-  width: 100%;
-  height: 100%;
-`;
-
 const HomePage = () => {
-  return (
-    <HomePageContainer>
-      <Map />
-    </HomePageContainer>
-  );
+  return <Map />;
 };
 
 export default HomePage;
